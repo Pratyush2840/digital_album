170a7ecea02e909143b02cf5c7d82daeed6636f8
Pratyush Singh <psingh08iph@gmail.com>
2026-07-27 23:35:38 +0530

    Extend Post model with user, likes and comments
    Adds an owning user reference, a likes array of user ids (used to
    toggle/count likes per user), an embedded comment subdocument
    (user + text + timestamps), and timestamps on the post itself.

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    text: { type: String, required: true, trim: true }
+}, { timestamps: true });
 
 const postSchema = new mongoose.Schema({
     image : String,
     caption : String,
-});
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    comments: [commentSchema]
+}, { timestamps: true });
 
 const Post = mongoose.model("Post", postSchema);
 module.exports = Post;
